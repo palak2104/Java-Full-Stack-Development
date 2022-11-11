d93f5e65347abe1f0a91c7f6886dbc133d452988
Palak Mittal <66310688+palak2104@users.noreply.github.com>
2022-11-12 04:20:00 +0530

    Create Nested classes.java

---
+++ Core Java OOPS/Nested classes.java
@@ -0,0 +1,38 @@
+/*a nested class can only be accessed by the reference of outer class, where the outer class cannot be static.
+Case 1: Nested class is Static
+if we need to access the inner static class we do not need the object of outer class, although we still need its reference with object declaration and defination.*/
+
+class outer{
+    static class inner{
+        String name;
+        inner(String name){
+            this.name=name;
+        }
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+		outer.inner i=new outer.inner("Palak");
+		System.out.println(i.name);
+	}
+}
+
+/*Case 2: if you want to access the non static inner class in that case we need to attach outer class reference with object declaration and outer class object with new.*/
+class outer{
+    class inner{
+        String name;
+        inner(String name){
+            this.name=name;
+        }
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+	    outer o=new outer();
+		outer.inner i=o.new inner("Palak");
+		System.out.println(i.name);
+	}
+}
+
